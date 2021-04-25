0e3af9c84a5025565a6efd1f1e3b4b8bb3e6b616
mostafa <mostafa.morsy@msa.edu.eg>
2021-04-25 17:57:41 +0200

    Ui Commit

---
+++ coffe_app/lib/main_test.dart
@@ -0,0 +1,23 @@
+import 'package:coffe_app/coffe_router.dart';
+import 'package:flutter/material.dart';
+
+import 'data_providers/auth_data_provider.dart';
+import 'data_providers/auth_provider.dart';
+import 'data_providers/http_client.dart';
+import './get_theme.dart';
+import 'screens/splash_screen.dart';
+
+void main() {
+  runApp(
+    AuthProvider(
+      auth: AuthDataProvider(http: HttpClient()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.system,
+        home: const SplashScreen(),
+        navigatorKey: CoffeeRouter.instance.navigatorKey,
+        theme: getTheme(),
+      ),
+    ),
+  );
+}
